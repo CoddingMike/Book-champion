d54e1d2d70158777307d001a768543ed3001832b
CoddingMike <maxi_faggiani@hotmail.com>
2022-09-19 01:59:37 -0300

    Clase 4

--- src/components/books/Bookitems/BookItem.js
+++ src/components/books/Bookitems/BookItem.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./BookItem.css";
 
 import ReadDate from "../ReadDate/ReadDate";
@@ -6,12 +8,19 @@ import BookCard from "../../ui/BookCard/BookCard";
 const BookItem = ({ title, author, dateRead, pageCount }) => {
   /* ----------------------> Object destructuring */
 
+  const [newTitle, setNewTitle] = useState(title);
+
+  const clickHandler = () => {
+    setNewTitle("Actualizado!");
+  };
+
   return (
     <BookCard>
-      <h2>{title}</h2>
+      <h2>{newTitle}</h2>
       <h3>{author}</h3>
       <ReadDate dateRead={dateRead} />
       <p>{pageCount}</p>
+      <button onClick={clickHandler}>Cambiar titulo</button>
     </BookCard>
   );
 };
